101f703904ce812209199a41ac32824eae096d95
zgfif <zihzag@gmail.com>
2017-08-17 20:04:12 +0300

    Added registration and log_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable, :validatable
 
 	has_many :books
 	has_many :locations, :through => :books
