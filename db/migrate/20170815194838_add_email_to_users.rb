1e906f96b221b30117cb14edfc46c2abf5ac0f43
zgfif <zihzag@gmail.com>
2017-08-18 19:15:56 +0300

    Modified migrations

--- db/migrate/20170815194838_add_email_to_users.rb
+++ db/migrate/20170815194838_add_email_to_users.rb
@@ -1,6 +1,6 @@
 class AddEmailToUsers < ActiveRecord::Migration[5.1]
   def change
-  	add_column :users, :email, :string, null: false, default: ""
+  	change_column :users, :email, :string, null: false, default: ""
   	add_index :users, :email,                unique: true
   end
 end
