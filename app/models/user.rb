bd1398cff08a7b10f1e8b395048a09d625fd5dee
zgfif <zihzag@gmail.com>
2017-08-19 13:10:51 +0300

    Modificated books index show

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,11 @@ class User < ApplicationRecord
 			
 		end		
 	def name_with_initial
-    	"#{name} #{surname}"    	
+		 unless name.nil?
+    	"#{name} #{surname} #{email}"
+    	 else
+    	 "#{email}"
+    	 end    	
   	end
    #skip_before_filter :verify_authenticity_token	
   #skip_before_action :verify_authenticity_token	
