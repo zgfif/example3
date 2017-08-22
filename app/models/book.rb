0d6735c649b32bef5262ec32c70816356972940f
zgfif <zihzag@gmail.com>
2017-08-22 09:37:43 +0300

    Add gem relationship diagrams

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,5 +5,12 @@ class Book < ApplicationRecord
   belongs_to :location
 
   belongs_to :holder, class_name: "User", foreign_key: 'holder_id', optional: true
- 
+  after_save :count_of_held_books
+
+
+  def count_of_held_books
+  	user = User.find_by(id: holder_id.to_i)
+  	user.held = user.held + 1
+  	user.save
+  end
 end
