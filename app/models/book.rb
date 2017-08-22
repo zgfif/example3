43c289083c742eb7421cd94db277ea35017924a5
zgfif <zihzag@gmail.com>
2017-08-22 10:49:53 +0300

    Calculating held count of user

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,12 +5,20 @@ class Book < ApplicationRecord
   belongs_to :location
 
   belongs_to :holder, class_name: "User", foreign_key: 'holder_id', optional: true
-  after_save :count_of_held_books
+  after_save :count_of_held_books2
 
 
   def count_of_held_books
-  	user = User.find_by(id: holder_id.to_i)
-  	user.held = user.held + 1
-  	user.save
+  	count = Book.where(holder)
+  	#user = User.find_by(id: holder_id.to_i)
+  	#user.held = user.held + 1
+  	#user.save
+  end
+  def count_of_held_books2
+  	users = User.all
+  		users.each do |user|
+  			user.held = Book.where(holder_id: user.id.to_i).count
+  			user.save
+  		end
   end
 end
