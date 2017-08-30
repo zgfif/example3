28c3061fb21a2eafcb9474fc514fe1aaa0451dff
zgfif <zihzag@gmail.com>
2017-08-30 20:40:58 +0300

    reconf production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,10 +89,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # Default Mailer Host
-  Rails.application.routes.default_url_options[:host] = 'booksget.herokuapp.com'
-  # Rails.application.routes.default_url_options = { :host => 'boiling-castle-79121.herokuapp.com' }
-    # config.action_mailer.delivery_method = :smtp
-    # config.action_mailer.perform_deliveries = true
-    # config.action_mailer.raise_delivery_errors = false
-    # config.action_mailer.default :charset => "utf-8"
+  # Rails.application.routes.default_url_options[:host] = 'booksget.herokuapp.com'
+  Rails.application.routes.default_url_options = { :host => 'booksget.herokuapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
 end
