78ff03330d75cd3770a58a64fe865e1264cf3a29
zgfif <zihzag@gmail.com>
2017-08-31 10:46:56 +0300

    config.action_mailer.raise_delivery_errors = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   # Default Mailer Host
   # Rails.application.routes.default_url_options[:host] = 'booksget.herokuapp.com'
-  config.action_mailer.raise_deliver_errors =true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'booksget.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
