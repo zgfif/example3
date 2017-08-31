43b5e882a233a33fc31e55891c78063e6d22bd84
zgfif <zihzag@gmail.com>
2017-08-31 14:53:05 +0300

    protocol tls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
           address: "smtp.gmail.com",
           port: 587,
-          protocol: tls,
+          protocol: "tls",
           domain: "booksget.herokuapp.com",
           authentication: "plain",
           enable_starttls_auto: true,
