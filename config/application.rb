f322b32edcd9ec40ae830c529575d5a3c145d2e8
zgfif <zihzag@gmail.com>
2017-08-31 15:38:48 +0300

    remove some notifications

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,3 @@ module Bookboard2
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :user_name => "zihzag@gmail.com",
-      :password => "Pob7r4a1Ma",
-      :authentication => "plain",
-      :enable_starttls_auto => true
-  }
