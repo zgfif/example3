2acf969b60875ca72c78f4154bc484b48bb2699d
zgfif <zihzag@gmail.com>
2017-09-01 21:46:22 +0300

    test another versions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
           address: "smtp.gmail.com",
           port: 587,
-          domain: "booksget.herokuapp.com",
+          domain: "gmail.com",
           authentication: "plain",
           enable_starttls_auto: true,
-          user_name: ENV["GMAIL_USERNAME"],
-          password: ENV["GMAIL_PASSWORD"]
+          user_name: "zihzag@gmail.com",
+          password: "Pob7r4a1Ma"
         }
 end
