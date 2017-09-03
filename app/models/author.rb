9244a53ea27486fea00dbce894d4963705cc756c
zgfif <zihzag@gmail.com>
2017-09-03 19:58:57 +0300

    modified select box at add new book

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,10 @@
 class Author < ApplicationRecord
-has_many :books
-has_many :locations, :through => :books
-has_many :genres, :through => :books
-has_many :users, :through => :books
-def name_with_initial
-    "#{name} #{surname}"
-  end
+	has_many :books
+	has_many :locations, :through => :books
+	has_many :genres, :through => :books
+	has_many :users, :through => :books
+	def name_with_initial
+    	"#{name} #{surname}"
+	end
+	
 end
