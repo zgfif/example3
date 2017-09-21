bfc26556458c71b7173731ebd4b747e7ee6a3411
zgfif <zihzag@gmail.com>
2017-09-21 18:48:53 +0300

    Made tables of index pages

--- app/controllers/admin/statistic_controller.rb
+++ app/controllers/admin/statistic_controller.rb
@@ -3,6 +3,7 @@ class Admin::StatisticController < Admin::AdminController
 		@user_count = User.all.count
 		@book_count = Book.all.count
 		@locations = Location.all
+		@count_locations = @locations.count
 		@book_available_count = Book.where(holder_id: nil).count
 		@book_held_count = Book.where(available: false).count
 		
